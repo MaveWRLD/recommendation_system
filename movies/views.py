8b41900bbe9db88ed14858be185790624bc06b91
Jacob Quaye <kofimave@gmail.com>
2025-09-07 18:11:55 +0000

    created the api views for the movie model

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework import generics, status, views
+from rest_framework.response import Response
+
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
 
 # Create your views here.
